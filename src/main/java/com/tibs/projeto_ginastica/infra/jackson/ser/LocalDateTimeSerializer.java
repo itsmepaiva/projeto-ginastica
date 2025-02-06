1caa6a03f632fe8b2f77cbfc39d692ee5ecdba67
paiva <saviopaivajr@gmail.com>
2025-02-06 19:08:04 -0300

    definiçao do banco de dados

---
+++ src/main/java/com/tibs/projeto_ginastica/infra/jackson/ser/LocalDateTimeSerializer.java
@@ -0,0 +1,22 @@
+package com.tibs.projeto_ginastica.infra.jackson.ser;
+
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import com.fasterxml.jackson.databind.ser.std.StdSerializer;
+import com.tibs.projeto_ginastica.infra.utils.JavaTimeUtils;
+
+import java.io.IOException;
+import java.time.LocalDateTime;
+
+public class LocalDateTimeSerializer extends StdSerializer<LocalDateTime> {
+    private static final long serialVersionUID = -6347158617481757931L;
+
+    public LocalDateTimeSerializer() {
+        super(LocalDateTime.class);
+    }
+
+    @Override
+    public void serialize(final LocalDateTime value, final JsonGenerator generator, final SerializerProvider provider) throws IOException {
+        generator.writeString(value.format(JavaTimeUtils.LOCAL_DATE_TIME_FORMATTER));
+    }
+}
